391a8edc9354d7cd5849baa35c23c78cbc4beaeb
Jony <jonyfortesok@gmail.com>
2021-08-30 01:28:44 -0300

    menu agregado

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NavBar.css'
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">TNA</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Mouse</a></li>
+                        <li><a href="badges.html">Teclados</a></li>
+                        <li><a href="collapsible.html">Monitores</a></li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
