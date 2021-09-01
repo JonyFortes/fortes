ea95fcda4cfc3e4aa939d074f99865cfbfe94e7f
Jony <jonyfortesok@gmail.com>
2021-08-31 21:30:56 -0300

    estilo al menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './NavBar.css';
 
 class Navbar extends React.Component{
     render(){
@@ -7,9 +8,9 @@ class Navbar extends React.Component{
                 <div class="nav-wrapper">
                     <a href="#" class="brand-logo">TNA</a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li><a href="sass.html">Mouse</a></li>
-                        <li><a href="badges.html">Teclados</a></li>
-                        <li><a href="collapsible.html">Monitores</a></li>
+                        <li><a class="menu-list" href="sass.html">Mouse</a></li>
+                        <li><a class="menu-list" href="badges.html">Teclados</a></li>
+                        <li><a class="menu-list" href="collapsible.html">Monitores</a></li>
                     </ul>
                 </div>
             </nav>
